c6f6cadb789411ca4e8379b8e04668d81bd7f15f
Ozan Türker Demir <ozanturkerdemir@gmail.com>
2018-06-09 23:07:26 +0300

    Create TermMatrix.py

---
+++ TermMatrix.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 22:30:39 2018
+
+@author: Dev_Ozan
+"""
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+docs = ['why hello there', 'omg hello pony omg', 'she went there? omg']
+vec = CountVectorizer()
+X = vec.fit_transform(docs)
+binaryX = X.toarray()
+
+for i in range(len(binaryX)):
+    for j in range(len(binaryX[i])):
+        if(binaryX[i][j] != 0):
+            binaryX[i][j] = 1
+
+df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+df2 = pd.DataFrame(binaryX, columns=vec.get_feature_names())
+
+print(df)
+print(df2)
