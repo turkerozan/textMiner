f5fab310d42f02dab980d4b73f22f940e858c8bc
Ozan Türker Demir <ozanturkerdemir@gmail.com>
2018-06-10 00:20:39 +0300

    Update TermMatrix.py

--- TermMatrix.py
+++ TermMatrix.py
@@ -7,18 +7,22 @@ Created on Sat Jun  9 22:30:39 2018
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
-docs = ['yol yapdı', 'aman tanrım o yea', 'free style mı kanka bu','hehe lahmacun yiyah','kılışdar yol yol yol Yapdı']
+docs = ['yol yapdı', 'aman tanrım o yea', 'free style mı kanka bu','hehe lahmacun yiyah','kılışdar yol yol yol Yapdı','bu']
+
 vec = CountVectorizer()
 X = vec.fit_transform(docs)
+df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names()) #frekans tablosu 
+print(df)
 binaryX = X.toarray()
-
 for i in range(len(binaryX)):
     for j in range(len(binaryX[i])):
         if(binaryX[i][j] != 0):
             binaryX[i][j] = 1
-
-df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
-df2 = pd.DataFrame(binaryX, columns=vec.get_feature_names())
-
-print(df)
+df2 = pd.DataFrame(binaryX, columns=vec.get_feature_names())#binary tablo
+vec =  CountVectorizer(analyzer='char',ngram_range=(3,3))
+X = vec.fit_transform(docs)
+df3 = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
 print(df2)
+print(df3)
+df = pd.concat([df, df2], axis=1, join_axes=[df.index])
+df = pd.concat([df, df3], axis=1, join_axes=[df.index])
